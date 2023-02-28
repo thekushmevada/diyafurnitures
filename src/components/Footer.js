fb4e300d0154be1e316099f2146e983dbc74b30a
thekushmevada <kush_20092@ldrp.ac.in>
2023-02-28 09:44:24 +0530

    changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -68,7 +68,7 @@ const Footer = () => {
             <hr />
             <div className="container grid grid-two-column ">
               <p>
-                @{new Date().getFullYear()} ThapaTechnical. All Rights Reserved
+                @{new Date().getFullYear()} Diya Furnitures. All Rights Reserved
               </p>
               <div>
                 <p>PRIVACY POLICY</p>
