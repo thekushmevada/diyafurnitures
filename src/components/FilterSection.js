46eef6c05314780e435464cbe51d01db1f3634c0
thekushmevada <kush_20092@ldrp.ac.in>
2023-03-01 12:55:09 +0530

    Products Page Added

---
+++ src/components/FilterSection.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FilterSection = () => {
+  return (
+    <div>
+      <h3>Hello from filtersection</h3>
+    </div>
+  )
+}
+
+export default FilterSection
---
+++ src/components/Sort.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Sort = () => {
+  return (
+    <div>
+            <h3>Hello from Sort</h3>
+
+    </div>
+  )
+}
+
+export default Sort
