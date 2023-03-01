54d092eefe8999c2b563059834f29070ca91608d
thekushmevada <kush_20092@ldrp.ac.in>
2023-03-01 18:25:35 +0530

    Sorting part done in Products Page

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -7,7 +7,7 @@ const FilterContext = createContext();
 const initialState = {
   filter_products: [],
   all_products: [],
-  grid_view: false,
+  grid_view: true,
   sorting_value: "lowest",
 };
 
@@ -21,16 +21,27 @@ export const FilterContextProvider = ({ children }) => {
   const setGridView = () => {
     return dispatch({ type: "SET_GRID_VIEW" });
   };
+  // to set the list view
   const setListView = () => {
     return dispatch({ type: "SET_LIST_VIEW" });
   };
+
+  // sorting function
+  const sorting = () => {
+    dispatch({type : "GET_SORT_VALUE"});
+  };
+
+  useEffect(() => {
+    dispatch({type:"SORTING_PRODUCTS", payload:products})
+  },[products,state.sorting_value])
+
   useEffect(() => {
     dispatch({ type: "LOAD_FILTER_PRODUCTS", payload: products });
   }, [products]);
 
   return (
     <FilterContext.Provider
-      value={{ ...state, setGridView, setListView /*, sorting */}}>
+      value={{ ...state, setGridView, setListView , sorting }}>
       {children}
     </FilterContext.Provider>
   );
--- src/reducer/filterReducer.js
+++ src/reducer/filterReducer.js
@@ -1,27 +1,66 @@
 const filterReducer = (state, action) => {
-    switch (action.type) {
-      case "LOAD_FILTER_PRODUCTS":
-        return {
-          ...state,
-          filter_products: [...action.payload],
-          all_products: [...action.payload],
-        };
-  
-      case "SET_GRID_VIEW":
-        return {
-          ...state,
-          grid_view: true,
-        };
+  switch (action.type) {
+    case "LOAD_FILTER_PRODUCTS":
+      return {
+        ...state,
+        filter_products: [...action.payload],
+        all_products: [...action.payload],
+      };
 
-        case "SET_LIST_VIEW":
-          return {
-            ...state,
-            grid_view: false,
-          };
-  
-      default:
-        return state;
-    }
-  };
-  
-  export default filterReducer;
+    case "SET_GRID_VIEW":
+      return {
+        ...state,
+        grid_view: true,
+      };
+
+    case "SET_LIST_VIEW":
+      return {
+        ...state,
+        grid_view: false,
+      };
+
+    case "GET_SORT_VALUE":
+      let userSortValue = document.getElementById("sort");
+      let sort_value = userSortValue.options[userSortValue.selectedIndex].value;
+      console.log(sort_value);
+      return {
+        ...state,
+        sorting_value: sort_value,
+      };
+
+    case "SORTING_PRODUCTS":
+      let newSortData;
+      let tempSortData = [...action.payload];
+
+      if (state.sorting_value === "lowest") {
+        const sortingProducts = (a,b) => {
+          return a.price - b.price;
+        }
+        newSortData = tempSortData.sort(sortingProducts);
+      }
+      if (state.sorting_value === "highest") {
+        const sortingProducts = (a,b) => {
+          return b.price - a.price;
+        }
+        newSortData = tempSortData.sort(sortingProducts);
+      }
+      if (state.sorting_value === "a-z") {
+        newSortData = tempSortData.sort((a, b) => {
+          return a.name.localeCompare(b.name);
+        });
+      }
+      if (state.sorting_value === "z-a") {
+        newSortData = tempSortData.sort((a, b) => {
+          return b.name.localeCompare(a.name);
+        });
+      }
+      return {
+        ...state,
+        filter_products: newSortData,
+      };
+    default:
+      return state;
+  }
+};
+
+export default filterReducer;
