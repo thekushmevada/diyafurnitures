2782def4b0e44067b1674dcfd8a349f45454dd75
thekushmevada <kush_20092@ldrp.ac.in>
2023-03-06 11:47:57 +0530

    Login & SignUp CSS Added

--- src/components/logincomponent.js
+++ src/components/logincomponent.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import styled from "styled-components";
+import { Button } from "../styles/Button";
 
 export default class Login extends Component {
-  
   constructor(props) {
     super(props);
     this.state = {
@@ -12,60 +13,66 @@ export default class Login extends Component {
   }
   handleSubmit(e) {
     e.preventDefault();
-    const { email , password } = this.state;
+    const { email, password } = this.state;
     // console.log(fname , lname , email , password);
     fetch("https://productssapi.onrender.com/login", {
-      method:"POST",
+      method: "POST",
       crossDomain: true,
       headers: {
-        "Content-Type" : "application/json",
+        "Content-Type": "application/json",
         Accept: "application/json",
         "Access-Control-Allow-Origin": "*",
       },
       body: JSON.stringify({
-        email , password
+        email,
+        password,
       }),
     })
-     .then((res) => res.json())
-    .then((data) => {
-      //console.log(data, "userRegister");
-      if (data.status === "ok") {
-        alert("login successful");
-        window.localStorage.setItem("token", data.data);
-        window.localStorage.setItem("loggedIn", true);
+      .then((res) => res.json())
+      .then((data) => {
+        //console.log(data, "userRegister");
+        if (data.status === "ok") {
+          alert("login successful");
+          window.localStorage.setItem("token", data.data);
+          window.localStorage.setItem("loggedIn", true);
 
-        window.location.href = "./userDetails";
-      }
-    })
+          window.location.href = "./userDetails";
+        }
+      });
   }
-
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <h3>Sign In</h3>
+      <Wrapper>
+        <div className="container contact-form">
+          <form onSubmit={this.handleSubmit} className="contact-inputs">
+            <h2 className="common-heading">Sign In</h2>
 
-        <div className="mb-3">
-          <label>Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-            onChange={(e) => this.setState({email : e.target.value})}
-          />
-        </div>
+            <div className="mb-3">
+              {/* <label>Email address</label> */}
+              {/* <br></br> */}
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Enter email"
+                onChange={(e) => this.setState({ email: e.target.value })}
+              />
+            </div>
 
-        <div className="mb-3">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-            onChange={(e) => this.setState({password : e.target.value})}
-          />
-        </div>
+            <div className="mb-3">
+              {/* <label className="common-heading">Password</label> */}
+              <br></br>
+              <br></br>
 
-        <div className="mb-3">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Enter password"
+                onChange={(e) => this.setState({ password: e.target.value })}
+              />
+            </div>
+
+            {/* <div className="mb-3">
           <div className="custom-control custom-checkbox">
             <input
               type="checkbox"
@@ -76,17 +83,53 @@ export default class Login extends Component {
               Remember me
             </label>
           </div>
+        </div> */}
+            <br></br>
+            <br></br>
+            <div className="d-grid">
+              <Button type="submit">Submit</Button>
+            </div>
+            <br></br>
+            <p className="forgot-password text-right">
+              Forgot <a href=" # ">password?</a>
+            </p>
+            <p className="forgot-password text-right">
+              Don't have an <a href="./register">account?</a>
+            </p>
+          </form>
         </div>
-
-        <div className="d-grid">
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </div>
-        <p className="forgot-password text-right">
-          Forgot <a href=" # ">password?</a>
-        </p>
-      </form>
-    )
+      </Wrapper>
+    );
   }
 }
+const Wrapper = styled.section`
+  padding: 9rem 0 5rem 0;
+  text-align: center;
+
+  .container {
+    margin-top: 6rem;
+
+    .contact-form {
+      max-width: 50rem;
+      margin: auto;
+
+      .contact-inputs {
+        display: flex;
+        flex-direction: column;
+        gap: 3rem;
+
+        input[type="submit"] {
+          cursor: pointer;
+          transition: all 0.2s;
+
+          &:hover {
+            background-color: ${({ theme }) => theme.colors.white};
+            border: 1px solid ${({ theme }) => theme.colors.btn};
+            color: ${({ theme }) => theme.colors.btn};
+            transform: scale(0.9);
+          }
+        }
+      }
+    }
+  }
+`;
--- src/components/signupcomponent.js
+++ src/components/signupcomponent.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import styled from "styled-components";
+import { Button } from "../styles/Button";
 
 export default class SignUp extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -15,76 +16,133 @@ export default class SignUp extends Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    const { fname , lname , email , password } = this.state;
+    const { fname, lname, email, password } = this.state;
     // console.log(fname , lname , email , password);
     fetch("https://productssapi.onrender.com/register", {
-      method:"POST",
+      method: "POST",
       crossDomain: true,
       headers: {
-        "Content-Type" : "application/json",
+        "Content-Type": "application/json",
         Accept: "application/json",
         "Access-Control-Allow-Origin": "*",
       },
       body: JSON.stringify({
-        fname , lname , email , password
+        fname,
+        lname,
+        email,
+        password,
       }),
-    }).then((res) => res.json())
-    .then((data) => {
-      console.log(data, "userRegister")
     })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data, "userRegister");
+      });
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <h3>Sign Up</h3>
+      <Wrapper>
+        <div className="container contact-form">
+          <form onSubmit={this.handleSubmit}>
+            <h2 className="common-heading">Sign Up</h2>
 
-        <div className="mb-3">
-          <label>First name</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="First name"
-            onChange={(e) => this.setState({fname: e.target.value})}
-          />
-        </div>
+            <div className="mb-3">
+              {/* <label>First name</label> */}
+              <input
+                type="text"
+                className="form-control"
+                placeholder="First name"
+                onChange={(e) => this.setState({ fname: e.target.value })}
+              />
+            </div>
 
-        <div className="mb-3">
-          <label>Last name</label>
-          <input type="text" className="form-control" placeholder="Last name" 
-          onChange={(e) => this.setState({lname: e.target.value})}  
-        />
-        </div>
+            <br></br>
+              <br></br>
 
-        <div className="mb-3">
-          <label>Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-            onChange={(e) => this.setState({email: e.target.value})}
-          />
-        </div>
+            <div className="mb-3">
+              {/* <label>Last name</label> */}
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Last name"
+                onChange={(e) => this.setState({ lname: e.target.value })}
+              />
+            </div>
 
-        <div className="mb-3">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-            onChange={(e) => this.setState({password: e.target.value})}
-          />
-        </div>
+            <br></br>
+              <br></br>
 
-        <div className="d-grid">
-          <button type="submit" className="btn btn-primary">
-            Sign Up
-          </button>
+            <div className="mb-3">
+              {/* <label>Email address</label> */}
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Enter email"
+                onChange={(e) => this.setState({ email: e.target.value })}
+              />
+            </div>
+
+            <br></br>
+              <br></br>
+
+            <div className="mb-3">
+              {/* <label>Password</label> */}
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Enter password"
+                onChange={(e) => this.setState({ password: e.target.value })}
+              />
+            </div>
+
+            <br></br>
+              <br></br>
+
+            <div className="d-grid">
+              <Button type="submit" className="btn btn-primary">
+                Sign Up
+              </Button>
+            </div>
+
+            <br></br>
+              
+            <p className="forgot-password text-right">
+              Already registered <a href="./login">sign in?</a>
+            </p>
+          </form>
         </div>
-        <p className="forgot-password text-right">
-          Already registered <a href="/login">sign in?</a>
-        </p>
-      </form>
-    )
+      </Wrapper>
+    );
   }
 }
+const Wrapper = styled.section`
+  padding: 9rem 0 5rem 0;
+  text-align: center;
+
+  .container {
+    margin-top: 6rem;
+
+    .contact-form {
+      max-width: 50rem;
+      margin: auto;
+
+      .contact-inputs {
+        display: flex;
+        flex-direction: column;
+        gap: 3rem;
+
+        input[type="submit"] {
+          cursor: pointer;
+          transition: all 0.2s;
+
+          &:hover {
+            background-color: ${({ theme }) => theme.colors.white};
+            border: 1px solid ${({ theme }) => theme.colors.btn};
+            color: ${({ theme }) => theme.colors.btn};
+            transform: scale(0.9);
+          }
+        }
+      }
+    }
+  }
+`;
